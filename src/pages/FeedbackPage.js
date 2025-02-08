e05d993f19b86eae92be1eab4e9be1bae3fa4379
Arora962 <157892517+Arora962@users.noreply.github.com>
2025-02-08 13:47:04 +0530

    deployment changes

--- src/pages/FeedbackPage.js
+++ src/pages/FeedbackPage.js
@@ -11,7 +11,7 @@ const FeedbackPage = () => {
 
   // Define backend URL dynamically for local and production
   const backendUrl = process.env.NODE_ENV === "production"
-  ? "https://indian-culture-ohanbx0fn-kriti-aroras-projects-46f3b49e.vercel.app"  // Replace with your actual backend URL
+  ? "https://indian-culture-backend-qdxibl6dn-kriti-aroras-projects-46f3b49e.vercel.app"  // Replace with your actual backend URL
   : "http://localhost:5001";  // Local URL for local development
 
   const handleBackClick = () => {
